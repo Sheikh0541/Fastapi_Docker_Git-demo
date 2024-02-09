6fce993a095503c4793c5e3f7229d7abcd29a2a2
Sheikh Iftekhar Ahmed <suborno0541@gmail.com>
2024-02-09 23:40:11 +0600

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI 
+from fastapi import UploadFile, File 
 import uvicorn
 
 app = FastAPI()
@@ -7,6 +8,14 @@ app = FastAPI()
 @app.get("/hello")
 def read_root():
     return {"hello": "world"} 
+
+@app.get("/")
+def hello_user(name: str):
+    return f"Hello: {name}"
+
+@app.post("/predict")
+def predict_image():
+    return f"Predicted type =" 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
