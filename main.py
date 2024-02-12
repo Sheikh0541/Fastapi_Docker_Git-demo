ab6eaccb8a3e0251e0fd1f7bc9c45ac59b17d3b2
Sheikh Iftekhar Ahmed <suborno0541@gmail.com>
2024-02-12 13:39:00 +0600

    Created model.py and Image Classification is working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI 
 from fastapi import UploadFile, File 
+from model import model_prediction, read_image
 import uvicorn
 
 app = FastAPI()
@@ -16,13 +17,19 @@ def hello_user(name: str):
 dir = "Images/"
 
 @app.post("/predict")
-async def predict_image(file: UploadFile = File(...)):
-        image = await file.read() 
+def predict_image(image: UploadFile):
+        object_image = image.file.read()
+        image = read_image(object_image) 
+        
+        # Making prediction result
+        result = model_prediction(image)
+        
+        # File Saving 
         file_name = "new.jpg" 
         with open(f"{dir}{file_name}", "wb") as f:
-             f.write(image) 
+             f.write(object_image) 
 
-        return  
+        return f"This is a {result}"  
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
