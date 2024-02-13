20b74bd745d64a8ab848c5dbd2512429835c4ada
mmk <mmkdhrubo@gmail.com>
2024-02-13 17:30:55 +0600

    working cat/dog classifier

--- Fresh_start/app/main.py
+++ Fresh_start/app/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from model import model_prediction, read_image
 
 app = FastAPI()
+
+dir = "Images/"
+
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World from a Dockerized fastAPI APP!"}
+
+
+@app.post("/predict")
+def predict_image(image: UploadFile):
+    object_image = image.file.read()
+    image = read_image(object_image)
+
+    # Making prediction result
+    result = model_prediction(image)
+
+    # File Saving
+    file_name = "new.jpg"
+    with open(f"{dir}{file_name}", "wb") as f:
+        f.write(object_image)
+
+    return f"This is a {result}"
---
+++ Fresh_start/app/model.py
@@ -0,0 +1,23 @@
+from keras.models import load_model
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import cv2
+
+model = load_model("cats_dogs_classifier.h5")
+
+
+def read_image(image_encoded):
+    pil_image = Image.open(BytesIO(image_encoded))
+    return np.array(pil_image)
+
+
+def model_prediction(image):
+    image = cv2.resize(image, (224, 224))
+    image = image.reshape(1, 224, 224, 3)
+    pred = model.predict(image)[0]
+    if pred[0] > pred[1]:
+        result = "cat"
+    else:
+        result = "dog"
+    return result  # Return as string
